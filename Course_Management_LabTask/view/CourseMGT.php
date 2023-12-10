dd16d8e2b7d5ea99842780b6c59007e6af2392d0
Pranta Hossen <prantahossen@gmail.com>
2023-12-10 16:24:43 +0600

    Lab Task-02

---
+++ Course_Management_LabTask/view/CourseMGT.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Course Management</title>
+</head>
+<body>
+    <table border="1" cellspacing="0" cellpadding="10" align="center">
+        <tr>
+            <td>
+                <h2>Course Management</h2>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
