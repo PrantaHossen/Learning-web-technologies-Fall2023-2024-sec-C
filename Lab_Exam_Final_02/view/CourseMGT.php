af4ba540210e861f89a84bef144d5d58fa8bd687
Pranta Hossen <prantahossen@gmail.com>
2023-12-10 21:18:50 +0600

    Final lab exam 02

--- Lab_Exam_Final_02/view/CourseMGT.php
+++ Lab_Exam_Final_02/view/CourseMGT.php
@@ -6,10 +6,32 @@
     <title>Course Management</title>
 </head>
 <body>
-    <table border="1" cellspacing="0" cellpadding="10" align="center">
+    <table border="1" cellspacing="0" cellpadding="10" align="center" width="100%">
+        <tr>
+            <td align="center">
+                <h2>Course Management</h2>
+            </td>
+        </tr>
+    </table>
+    <table border="1" cellspacing="0" cellpadding="40" align="left">
         <tr>
             <td>
-                <h2>Course Management</h2>
+                <a href="AddingCourse.php">Add Course</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href="ViewCourses.php">Update Course</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href="ViewCourses.php">Delete Course</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href="ViewCourses.php">View Courses</a>
             </td>
         </tr>
     </table>
